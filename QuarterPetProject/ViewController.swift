f3185ee018465f7f4c9dbd843c05bc316963909e
Lirivit <lirikit323@gmail.com>
2021-09-12 14:00:15 +0300

    Initial Commit

---
+++ QuarterPetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuarterPetProject
+//
+//  Created by Kirill Fokov on 12.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
